0bbe9bd1feed178e7f3f45e4f4922c0961c9fada
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-10-27 04:42:06 +0200

    api-docs

---
+++ docs/swaggerDoc.js
@@ -0,0 +1,95 @@
+export const swaggerDocument = {
+    swaggerDefinition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Receipto API',
+        version: '1.0.0',
+        description: 'API documentation for Receipto',
+      },
+      servers: [
+        {
+          url: `http://127.0.0.1:${process.env.PORT || 3000}`,
+        },
+      ],
+      components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+          },
+        },
+        schemas: {
+          Receipt: {
+            type: 'object',
+            properties: {
+              id: {
+                type: 'string',
+                description: 'Unique identifier for the receipt',
+              },
+              filename: {
+                type: 'string',
+                description: 'The name of the uploaded receipt file',
+              },
+              fileUrl: {
+                type: 'string',
+                description: 'The URL of the uploaded receipt file',
+              },
+              metadata: {
+                type: 'object',
+                properties: {
+                  date: {
+                    type: 'string',
+                    format: 'date-time',
+                    description: 'Date of the transaction',
+                  },
+                  amount: {
+                    type: 'number',
+                    description: 'Amount spent in the transaction',
+                  },
+                  category: {
+                    type: 'string',
+                    description: 'Category of the transaction',
+                  },
+                },
+              },
+            },
+          },
+          CreateReceipt: {
+            type: 'object',
+            required: ['filename', 'metadata'],
+            properties: {
+              filename: {
+                type: 'string',
+                description: 'The name of the uploaded receipt file',
+              },
+              metadata: {
+                type: 'object',
+                properties: {
+                  date: {
+                    type: 'string',
+                    format: 'date-time',
+                    description: 'Date of the transaction',
+                  },
+                  amount: {
+                    type: 'number',
+                    description: 'Amount spent in the transaction',
+                  },
+                  category: {
+                    type: 'string',
+                    description: 'Category of the transaction',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    apis: ['./routes/*.js'], // path to API docs
+  };
+
+export const options = {
+  customCss: '.swagger-ui .topbar {display: none }',
+};
+
--- server.js
+++ server.js
@@ -3,41 +3,14 @@ import router from './routes/index.js';
 import logger from './middleware/logger.js';
 import { errorHandler, JsonErrorHandler } from './middleware/error.js';
 import { notFound } from './middleware/notFound.js';
-import swaggerUi from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import {swaggerDocument, options} from './docs/swaggerDoc.js';
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-const swaggerOptions = {
-    swaggerDefinition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Receipt management Restful API',
-            version: '1.0.0',
-            description: 'API documentation using swagger',
-        },
-        servers: [
-            {
-                url: `http://127.0.0.1:${port}`,
-            },
-        ],
-        components: {
-            securitySchemes: {
-                bearerAuth: {
-                    type: 'http',
-                    scheme: 'bearer',
-                    bearerFormat: 'JWT',
-                },
-            },
-        },
-    },
-    apis: ['./routes/*.js'], // path to API docs
-};
-const options = {
-    customCss: '.swagger-ui .topbar {display: none }',
-};
-const swaggerDocs = swaggerJSDoc(swaggerOptions);
+const swaggerDocs = swaggerJSDoc(swaggerDocument);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, options));
 
 app.use(logger);
